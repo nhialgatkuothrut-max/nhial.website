726433bf600a10adb05284f22b12b2e8e0e02a4a
nhialgatkuothrut-max <nhialgatkuothrut@gmail.com>
2026-08-05 18:23:59 +0300

    Create Messages component to display contact messages
    Signed-off-by: nhialgatkuothrut-max <nhialgatkuothrut@gmail.com>

---
+++ messages.jsx
@@ -0,0 +1,32 @@
+function Messages(){
+
+return(
+
+<section>
+
+<h2>
+Contact Messages
+</h2>
+
+
+<div className="message">
+
+<p>
+Name: Visitor
+</p>
+
+<p>
+Message: Your portfolio is amazing
+</p>
+
+
+</div>
+
+
+</section>
+
+)
+
+}
+
+export default Messages;
