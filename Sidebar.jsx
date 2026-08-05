d779d9ba62c2736373cd610bdf727549bface98f
nhialgatkuothrut-max <nhialgatkuothrut@gmail.com>
2026-08-05 18:20:54 +0300

    Add Sidebar component with navigation links
    Signed-off-by: nhialgatkuothrut-max <nhialgatkuothrut@gmail.com>

---
+++ Sidebar.jsx
@@ -0,0 +1,32 @@
+function Sidebar(){
+
+return(
+
+<div className="sidebar">
+
+<h2>
+ADMIN
+</h2>
+
+
+<ul>
+
+<li>Dashboard</li>
+
+<li>Projects</li>
+
+<li>Messages</li>
+
+<li>Profile</li>
+
+<li>Logout</li>
+
+</ul>
+
+</div>
+
+)
+
+}
+
+export default Sidebar;
