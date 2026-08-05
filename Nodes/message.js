4114a68a61b53cce70a807dbca544ef765d3c523
nhialgatkuothrut-max <nhialgatkuothrut@gmail.com>
2026-08-05 18:10:55 +0300

    Create Message model with schema for messages
    Signed-off-by: nhialgatkuothrut-max <nhialgatkuothrut@gmail.com>

---
+++ Nodes/message.js
@@ -0,0 +1,24 @@
+const mongoose=require("mongoose");
+
+
+const MessageSchema=new mongoose.Schema({
+
+name:String,
+
+email:String,
+
+message:String,
+
+date:{
+type:Date,
+default:Date.now
+}
+
+});
+
+
+module.exports =
+mongoose.model(
+"Message",
+MessageSchema
+);
